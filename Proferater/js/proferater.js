9a12f5861e7d3d3249a3f34ed00caf2f70166e8b
ElishaG <elishajeremy.gonzales@gmail.com>
2014-02-25 17:19:15 +0800

    Proferate Button Fixed
    Working on the summary of ratings

--- Proferater/js/proferater.js
+++ Proferater/js/proferater.js
@@ -2,3 +2,17 @@ $("#info").popover({
 	placement: "bottom",
 	trigger: "click",
 });
+
+$('.btn-proferate').on('click', function(e) {
+	alert("hi!");
+})
+
+/*
+$(document).on('pagebeforeshow', '#prof-page', function(){
+	$("#btn-prof").on('click', function(e) {
+		alert("hi");
+	});
+});
+
+
+*/
