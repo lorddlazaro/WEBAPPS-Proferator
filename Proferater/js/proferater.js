3377d23540b267239288f2600b13dc5d4d0bf569
ElishaG <elishajeremy.gonzales@gmail.com>
2014-02-25 20:26:41 +0800

    Updated rate buttons

--- Proferater/js/proferater.js
+++ Proferater/js/proferater.js
@@ -2,17 +2,3 @@ $("#info").popover({
 	placement: "bottom",
 	trigger: "click",
 });
-
-$('.btn-proferate').on('click', function(e) {
-	alert("hi!");
-})
-
-/*
-$(document).on('pagebeforeshow', '#prof-page', function(){
-	$("#btn-prof").on('click', function(e) {
-		alert("hi");
-	});
-});
-
-
-*/
