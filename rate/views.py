edcdf3efeed64b8a0065ff20edf67814ae72f5d6
pauleyy <pauletteconstantino@gmail.com>
2014-04-07 07:56:56 +0800

    testing view

--- rate/views.py
+++ rate/views.py
@@ -39,5 +39,10 @@ def rate(request, professor_id):
     return HttpResponse("You're Looking at the professor rate form %s" % professor_id)
 
 def signup(request):
-    form = SignUpForm
+    form = SignUpForm(request.POST or None)
+
+    if form.is_valid:
+        save_it = form.save(commit=False)
+        save_it.save()
+
     return render_to_response("signup.html", locals(), context_instance=RequestContext(request))
